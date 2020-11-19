8d07083553dbf82cff764c1203446fa425d2331b
Ian MacLeod <imac@Ians-MBP.fios-router.home>
2020-11-19 17:02:42 -0500

    Cleaned up code and added comments

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,22 @@ public class Main {
 	}
 	public int[] twoSum(int[] nums, int target) {
 		
+		//created empty array of size two, since the list returned will have two integers in it
 		int[] answer = new int[2];
 		
+		//embedded for loop; only looks at sums where i and j are not the same so as to avoid the same indices being added to the answer array
 		for(int i = 0; i < nums.length; i++) {
 			for(int j = 0; j < nums.length; j++) {
 				if(i != j) {
-					if(nums[i] + nums[j] == target) {
-						answer[0] = j;
-						answer[1] = i;
+					if(nums[i] + nums[j] == target) { //if the two numbers from the array equal the target value...
+						answer[0] = j; //set first index of answer array to j's value
+						answer[1] = i; //set second index of answer array to i's value
 					}
 				}
-			
 			}
 		}
 		
+		//return array 
 		return answer;
 	}
 	
